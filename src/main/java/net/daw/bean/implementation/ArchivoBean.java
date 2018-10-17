5a05eb19cc369b9a67a889d55c180a36e92ca02c
USUARIO <USUARIO@USUARIO-PC>
2018-10-17 08:32:32 +0200

    Añadir Tabla Archivo

---
+++ src/main/java/net/daw/bean/implementation/ArchivoBean.java
@@ -0,0 +1,166 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.daw.bean.implementation;
+
+import com.google.gson.annotations.Expose;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
+import net.daw.bean.publicinterface.GenericBean;
+import net.daw.dao.implementation.ObraDao;
+import net.daw.helper.statics.EncodingUtilHelper;
+
+public class ArchivoBean implements GenericBean {
+
+    @Expose
+    private Integer id = 0;
+    @Expose
+    private Date alta;
+    @Expose
+    private String origen;
+    @Expose
+    private String estado;
+    
+    @Expose(serialize = false)
+    private Integer id_obra; 
+    @Expose(deserialize = false)
+    private ObraBean obj_obra = null;   
+    
+    public ArchivoBean() {
+    }
+
+    public ArchivoBean(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getAlta() {
+        return alta;
+    }
+
+    public void setAlta(Date alta) {
+        this.alta = alta;
+    }
+
+    public String getOrigen() {
+        return origen;
+    }
+
+    public void setOrigen(String origen) {
+        this.origen = origen;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public Integer getId_obra() {
+        return id_obra;
+    }
+
+    public void setId_obra(Integer id_obra) {
+        this.id_obra = id_obra;
+    }
+    
+    public ObraBean getObj_obra() {
+        return obj_obra;
+    }
+
+    public void setObj_obra(ObraBean obj_obra) {
+        this.obj_obra = obj_obra;
+    }
+
+    
+    @Override
+    public String getColumns() {
+        String strColumns = "";
+        strColumns += "id,";
+        strColumns += "alta,";
+        strColumns += "origen,";
+        strColumns += "estado,";
+        strColumns += "id_obra";
+        return strColumns;
+    }
+
+    @Override
+    public String getValues() {
+        String strColumns = "";
+        strColumns += id + ",";
+        //strColumns += EncodingUtilHelper.stringifyAndQuotate(fecha);
+        /*Hay que comprobar si la fecha es null porque si es null inserta la fecha actual del sistema*/
+        if (alta == null) {
+            strColumns += "null";
+        } else {
+            strColumns += EncodingUtilHelper.stringifyAndQuotate(alta);
+        }
+        strColumns += EncodingUtilHelper.quotate(getOrigen()) + ",";
+        strColumns += EncodingUtilHelper.quotate(getEstado()) + ",";
+        strColumns += id_obra;        
+        return strColumns;
+    }
+
+    @Override
+    public String toPairs() {
+        String strPairs = ""; 
+//        strPairs += "alta=" + EncodingUtilHelper.stringifyAndQuotate(alta);       
+//        strPairs += "origen=" + EncodingUtilHelper.quotate(getOrigen()) + ",";
+//        strPairs += "estado=" + EncodingUtilHelper.quotate(getEstado()) + ",";
+//        strPairs += "id_obra=" + getObra();
+        Boolean hay = false;
+        if (alta != null) {
+            strPairs += "alta=" + EncodingUtilHelper.stringifyAndQuotate(alta);
+            hay = true;
+        }
+        if (origen != null) {
+            strPairs += (hay) ? ",origen=" : "origen=";
+            strPairs += EncodingUtilHelper.quotate(getOrigen());
+            hay = true;
+        }
+        if (estado != null) {
+            strPairs += (hay) ? ",estado=" : "estado=";
+            strPairs += EncodingUtilHelper.quotate(getEstado());
+            hay = true;
+        }
+        if (id_obra != null) {
+            strPairs += (hay) ? ",id_obra=" : "id_obra=";
+            strPairs += getId_obra();
+        }
+        return strPairs;
+    }
+
+    @Override
+    public ArchivoBean fill(ResultSet oResultSet, Connection pooledConnection, PusuarioBean oPusuarioBean_security, Integer expand) throws SQLException, Exception {
+        this.setId(oResultSet.getInt("id"));
+        this.setAlta(oResultSet.getTimestamp("alta"));
+        this.setOrigen(oResultSet.getString("origen"));
+        this.setEstado(oResultSet.getString("estado"));
+        
+        if (expand > 0) {
+            ObraBean oObraBean = new ObraBean();
+            ObraDao oObraDao = new ObraDao(pooledConnection, oPusuarioBean_security, null);
+            oObraBean.setId(oResultSet.getInt("id_obra"));
+            oObraBean = oObraDao.get(oObraBean, expand - 1);
+            this.setObj_obra(oObraBean);
+        } else {
+            this.setId_obra(oResultSet.getInt("id_obra"));
+        }
+
+        return this;
+    }
+
+}
---
+++ src/main/java/net/daw/dao/implementation/ArchivoDao.java
@@ -0,0 +1,169 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.daw.dao.implementation;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.HashMap;
+import net.daw.bean.implementation.PusuarioBean;
+import net.daw.bean.implementation.ArchivoBean;
+import net.daw.dao.publicinterface.TableDaoInterface;
+import net.daw.dao.publicinterface.ViewDaoInterface;
+import net.daw.data.implementation.MysqlData;
+import net.daw.helper.statics.FilterBeanHelper;
+import net.daw.helper.statics.Log4j;
+import net.daw.helper.statics.SqlBuilder;
+
+public class ArchivoDao implements ViewDaoInterface<ArchivoBean>, TableDaoInterface<ArchivoBean> {
+
+    private String strTable = "archivo";
+    private String strSQL = "select * from " + strTable + " where 1=1 ";
+    private MysqlData oMysql = null;
+    private Connection oConnection = null;
+    private PusuarioBean oPuserSecurity = null;
+
+    public ArchivoDao(Connection oPooledConnection, PusuarioBean oPuserBean_security, String strWhere) throws Exception {
+        try {
+            oConnection = oPooledConnection;
+            oMysql = new MysqlData(oConnection);
+            oPuserSecurity = oPuserBean_security;
+            if (strWhere != null) {
+                strSQL += strWhere;
+            }
+        } catch (Exception ex) {
+            Log4j.errorLog(this.getClass().getName() + ":" + (ex.getStackTrace()[0]).getMethodName(), ex);
+            throw new Exception();
+        }
+    }
+
+    @Override
+    public Long getCount(ArrayList<FilterBeanHelper> hmFilter) throws Exception {
+        strSQL += SqlBuilder.buildSqlWhere(hmFilter);
+        Long pages = 0L;
+        try {
+            pages = oMysql.getCount(strSQL);
+        } catch (Exception ex) {
+            Log4j.errorLog(this.getClass().getName() + ":" + (ex.getStackTrace()[0]).getMethodName(), ex);
+            throw new Exception();
+        }
+        return pages;
+    }
+
+    @Override
+    public ArrayList<ArchivoBean> getPage(int intRegsPerPag, int intPage, ArrayList<FilterBeanHelper> alFilter, HashMap<String, String> hmOrder, Integer expand) throws Exception {
+        strSQL += SqlBuilder.buildSqlWhere(alFilter);
+        strSQL += SqlBuilder.buildSqlOrder(hmOrder);
+        strSQL += SqlBuilder.buildSqlLimit(oMysql.getCount(strSQL), intRegsPerPag, intPage);
+        ArrayList<ArchivoBean> arrUser = new ArrayList<>();
+        ResultSet oResultSet = null;
+        try {
+            oResultSet = oMysql.getAllSQL(strSQL);
+            while (oResultSet.next()) {
+                ArchivoBean oArchivoBean = new ArchivoBean();
+                arrUser.add((ArchivoBean) oArchivoBean.fill(oResultSet, oConnection, oPuserSecurity, expand));
+            }
+            if (oResultSet != null) {
+                oResultSet.close();
+            }
+        } catch (Exception ex) {
+            Log4j.errorLog(this.getClass().getName() + ":" + (ex.getStackTrace()[0]).getMethodName(), ex);
+            throw new Exception();
+        } finally {
+            if (oResultSet != null) {
+                oResultSet.close();
+            }
+        }
+        return arrUser;
+    }
+
+    @Override
+    public ArrayList<ArchivoBean> getAll(ArrayList<FilterBeanHelper> alFilter, HashMap<String, String> hmOrder, Integer expand) throws Exception {
+        strSQL += SqlBuilder.buildSqlWhere(alFilter);
+        strSQL += SqlBuilder.buildSqlOrder(hmOrder);
+        ArrayList<ArchivoBean> arrUser = new ArrayList<>();
+        ResultSet oResultSet = null;
+        try {
+            oResultSet = oMysql.getAllSQL(strSQL);
+            while (oResultSet.next()) {
+                ArchivoBean oArchivoBean = new ArchivoBean();
+                arrUser.add((ArchivoBean) oArchivoBean.fill(oResultSet, oConnection, oPuserSecurity, expand));
+            }
+        } catch (Exception ex) {
+            Log4j.errorLog(this.getClass().getName() + ":" + (ex.getStackTrace()[0]).getMethodName(), ex);
+            throw new Exception();
+        } finally {
+            if (oResultSet != null) {
+                oResultSet.close();
+            }
+        }
+        return arrUser;
+    }
+
+    @Override
+    public ArchivoBean get(ArchivoBean oArchivoBean, Integer expand) throws Exception {
+        if (oArchivoBean.getId() > 0) {
+            ResultSet oResultSet = null;
+            try {
+                oResultSet = oMysql.getAllSQL(strSQL + " And id= " + oArchivoBean.getId() + " ");
+                Boolean empty = true;
+                while (oResultSet.next()) {
+                    oArchivoBean = (ArchivoBean) oArchivoBean.fill(oResultSet, oConnection, oPuserSecurity, expand);
+                    empty = false;
+                }
+                if (empty) {
+                    oArchivoBean.setId(0);
+                }
+            } catch (Exception ex) {
+                Log4j.errorLog(this.getClass().getName() + ":" + (ex.getStackTrace()[0]).getMethodName(), ex);
+                throw new Exception();
+            } finally {
+                if (oResultSet != null) {
+                    oResultSet.close();
+                }
+            }
+        } else {
+            oArchivoBean.setId(0);
+        }
+        return oArchivoBean;
+    }
+
+    @Override
+    public Integer set(ArchivoBean oArchivoBean) throws Exception {
+        Integer iResult = null;
+        try {
+            if (oArchivoBean.getId() == 0) {
+                strSQL = "INSERT INTO " + strTable + " ";
+                strSQL += "(" + oArchivoBean.getColumns() + ")";
+                strSQL += "VALUES(" + oArchivoBean.getValues() + ")";
+                iResult = oMysql.executeInsertSQL(strSQL);
+            } else {
+                strSQL = "UPDATE " + strTable + " ";
+                strSQL += " SET " + oArchivoBean.toPairs();
+                strSQL += " WHERE id=" + oArchivoBean.getId();
+                iResult = oMysql.executeUpdateSQL(strSQL);
+            }
+        } catch (Exception ex) {
+            Log4j.errorLog(this.getClass().getName() + ":" + (ex.getStackTrace()[0]).getMethodName(), ex);
+            throw new Exception();
+        }
+        return iResult;
+    }
+
+    @Override
+    public Integer remove(Integer id) throws Exception {
+        int result = 0;
+        try {
+            result = oMysql.removeOne(id, strTable);
+        } catch (Exception ex) {
+            Log4j.errorLog(this.getClass().getName() + ":" + (ex.getStackTrace()[0]).getMethodName(), ex);
+            throw new Exception();
+        }
+        return result;
+    }
+
+}
+
